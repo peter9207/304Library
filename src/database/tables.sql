750cabdc3fde1d8785fac70bf6e0fb7f387777c7
Peter Zhang <peter920730@gmail.com>
2012-11-10 00:14:09 -0800

    made more table creation scripts

--- src/database/tables.sql
+++ src/database/tables.sql
@@ -1,25 +1,56 @@
-create table BORROWER (
-			NUMBER BID,
-			NUMBER PASSWORD,
-			VARCHAR2 NAME,
-			VARCHAR2 ADDRESS,
-			NUMBER PHONE,
-			VARCHAR2 EMAILADDRESS,
-			NUMBER SINORSTNO,
-			VARCHAR2 TYPE
-);
+create table borrower(
+	NUMBER bid primary key,
+	VARCHAR2 password not null,
+	varchar2 name,
+	varchar2 address,
+	varchar2 emailAddress, 
+	number sinOrStNo, 
+	timestamp expiryDate, 
+	varchar2 type);
+	
+create table borrower_type(
+	varchar2 type primary key ,
+	timestamp bookTimeLimit);
+	
+create table Book (
+	number callNumber primary key , 
+	number isbn,
+	varchar2 title, 
+	varchar2 mainAuthor, 
+	varchar2 publisher, 
+	timestamp year );
+	
+create table HasAuthor (
+	number callNumber primary key, 
+	varchar2 name);
 
-create table BORROWERTYPE(
-				varchar2 type,
-				booktimelimit
-);
-
-create table book(
-				number callnumber,
-				number isbn,
-				varchar2 title,
-				varchar2 mainauthor,
-				varchar2 publisher,
-				date year
-				);
-				
+create table HasSubject (
+	number callNumber, 
+	varchar2 subject,
+	primary key(callNumber, subject));
+	
+create table BookCopy (
+	number callNumber, 
+	number copyNo, 
+	varchar2 status);
+	
+create table HoldRequest(
+	number hid, 
+	number bid, 
+	number callNumber, 
+	timestamp issuedDate);
+	
+create table Borrowing(
+	number borid, 
+	number bid, 
+	number callNumber, 
+	number copyNo, 
+	timestamp outDate, 
+	timestamp inDate);
+	
+create table Fine (
+	number fid, 
+	number amount, 
+	timestamp issuedDate, 
+	timestamp paidDate, 
+	number borid);
