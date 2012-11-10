5c01db130614f2011df8fabfbd5a92d70f65d1aa
Peter Zhang <peter920730@gmail.com>
2012-11-10 00:48:36 -0800

    added main frame class

---
+++ src/ui/MainFrame.java
@@ -0,0 +1,23 @@
+package ui;
+
+import javax.swing.*;
+/**
+ * 
+ * @author peter9207
+ *
+ * Class for the main window of the library program
+ */
+public class MainFrame extends JFrame{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final String FRAME_TITLE = "Library Console";
+	
+	public MainFrame (){
+		super(FRAME_TITLE);
+		
+	}
+
+}
