1ef5ce421b3d809308c118b3b0cab682ce7c0173
Peter Zhang <peter920730@gmail.com>
2012-11-12 14:40:05 -0800

    added main method

---
+++ src/main/MainLibrary.java
@@ -0,0 +1,14 @@
+package main;
+
+import javax.swing.JFrame;
+
+import ui.MainFrame;
+
+public class MainLibrary {
+	public static void main(String [] args){
+		JFrame frame = new MainFrame();
+		frame.pack();
+		frame.setVisible(true);
+	}
+
+}
